3e15ff7a06550502c3827533537a171d37999290
theo.sellem <theo.sellem@hotmail.fr>
2010-12-29 02:48:55 +0000

    J'me suis planté, là ya tout, j'crois Je m'aime.

---
+++ Yello Killer/YelloKiller/MapEditor/MapEdit.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.IO;
+
+
+namespace Yellokiller
+{
+    class MapEdit
+    {
+        public int[,] map = new int[Taille_Map.HAUTEURMAP, Taille_Map.LARGEURMAP];
+        public int largeurMap = Taille_Map.LARGEURMAP, hauteurMap = Taille_Map.HAUTEURMAP;
+
+        public MapEdit()
+        {
+        }
+
+        private Texture2D LoadContent(ContentManager content, string assetName)
+        {
+            return content.Load<Texture2D>(assetName);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, ContentManager content)
+        {
+            for (int y = 0; y < hauteurMap; y++)
+            {
+                for (int x = 0; x < largeurMap; x++)
+                {
+                    switch (map[y, x])
+                    {
+                        case 0:
+                            spriteBatch.Draw(LoadContent(content, "herbe"), new Vector2(x * 28, y * 28), Color.White);
+                            break;
+                        case 1:
+                            spriteBatch.Draw(LoadContent(content, "arbre"), new Vector2(x * 28, y * 28), Color.White);
+                            break;
+                        case 2:
+                            spriteBatch.Draw(LoadContent(content, "mur"), new Vector2(x * 28, y * 28), Color.White);
+                            break;
+                        case 3:
+                            spriteBatch.Draw(LoadContent(content, "maison"), new Vector2(x * 28, y * 28), Color.White);
+                            break;
+                        case 4:
+                            spriteBatch.Draw(LoadContent(content, "arbre2"), new Vector2(x * 28, y * 28), Color.White);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Yello Killer/YelloKiller/Screens/PauseMenuScreen.cs
@@ -0,0 +1,138 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// PauseMenuScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Yellokiller
+{
+    /// <summary>
+    /// The pause menu comes up over the top of the game,
+    /// giving the player options to resume or quit.
+    /// </summary>
+    class PauseMenuScreen : MenuScreen
+    {
+        int mod;
+
+        #region Initialization
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PauseMenuScreen(int comingfrom, int mode)
+            : base("En pause")
+        {
+            //Conserve la sélection
+            selectedEntry = comingfrom;
+
+            mod = mode;
+
+            // Create our menu entries.
+            MenuEntry resumeGameMenuEntry;
+            MenuEntry optionsGameMenuEntry = new MenuEntry("Options");
+            MenuEntry quitGameMenuEntry;
+
+            if (mode == 1)
+            {
+                resumeGameMenuEntry = new MenuEntry("Reprendre l'édition");
+                quitGameMenuEntry = new MenuEntry("Quitter l'édition");
+            }
+            else
+            {
+                resumeGameMenuEntry = new MenuEntry("Reprendre le jeu");
+                quitGameMenuEntry = new MenuEntry("Quitter la partie");
+            }
+
+
+            // Hook up menu event handlers.
+            resumeGameMenuEntry.Selected += OnCancel;
+            optionsGameMenuEntry.Selected += OptionsMenuEntrySelected;
+            quitGameMenuEntry.Selected += QuitGameMenuEntrySelected;
+
+            // Add entries to the menu.
+            MenuEntries.Add(resumeGameMenuEntry);
+            MenuEntries.Add(optionsGameMenuEntry);
+            MenuEntries.Add(quitGameMenuEntry);
+        }
+
+
+        #endregion
+
+        #region Handle Input
+
+
+        /// <summary>
+        /// Event handler for when the Quit Game menu entry is selected.
+        /// </summary>
+        void QuitGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            string message;
+
+            if (mod == 1)
+                message = "Êtes-vous sûr de vouloir quitter l'édition?\nToute édition non sauvegardée sera perdue.";
+            else
+                message = "Êtes-vous sûr de vouloir quitter la partie?";
+
+            MessageBoxScreen confirmQuitMessageBox = new MessageBoxScreen(message);
+
+            confirmQuitMessageBox.Accepted += ConfirmQuitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmQuitMessageBox, ControllingPlayer);
+        }
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+
+        public void OptionsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new OptionsMenuScreen(mod), e.PlayerIndex, true);
+            ScreenManager.RemoveScreen(this);
+        }
+
+        /// <summary>
+        /// Event handler for when the user selects ok on the "are you sure
+        /// you want to quit" message box. This uses the loading screen to
+        /// transition from the game back to the main menu screen.
+        /// </summary>
+        void ConfirmQuitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, false, null, new BackgroundScreen(),
+                                                           new MainMenuScreen());
+            MediaPlayer.Stop();
+        }
+
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            base.OnCancel(playerIndex);
+            Pausebckground.Quit(playerIndex, ScreenManager);
+        }
+
+
+        #endregion
+
+        #region Draw
+
+
+        /// <summary>
+        /// Draws the pause menu screen. This chains to the base MenuScreen.Draw.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+
+        #endregion
+    }
+}
